3a4e79fca37f7e0f93a808ac02949ca5346a0b5e
kakanjau <gogs@fake.local>
2016-10-12 17:52:52 +0800

    publish 1.0.1

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import doT from "dot";
  * dot template
  * @type {Class}
  */
-
 let Base = think.adapter('template', 'base');
-
 
 global.dot = global.dot || {};
 global.dot.filter = global.dot.filter || {};
@@ -17,30 +15,49 @@ global.dot.default = global.dot.default || {
   endSymbol: '}}'
 };
 
-function loadfile(filePath) {
+let dotDef = {
+  loadfile: fnLoadfile
+};
+
+function fnLoadfile(filePath) {
   var data = fs.readFileSync(filePath);
   if (data) return data.toString();
 }
 
-export default class extends Base {
+export default class ThinkTemplateDot extends Base {
   /**
    * run
    * @param  {String} templateFile [template filepath]
    * @param  {Object} tVar         [data]
    * @return {String}              []
    */
+
+  static reconfig(def = dotDef, dot = global.dot) {
+    dotDef = def;
+    global.dot = dot;
+    dotDef.loadfile = dotDef.loadfile || fnLoadfile;
+    thinkCache(thinkCache.VIEW_CONTENT, null);
+  }
+
   async run(templateFile, tVar, config) {
     let dotConfig = config && config.adapter && config.adapter.dot || {};
-    let dotDef = {
-      loadfile: loadfile
-    };
+
     let options = this.parseConfig(doT.templateSettings, dotConfig);
 
     this.prerender(options, doT, dotDef, global.dot);
-
+    
+    if(options.cache_compile) {
+      let doTCompiled = thinkCache(thinkCache.VIEW_CONTENT, templateFile + '-compile');
+      if (doTCompiled) {
+        return doTCompiled(tVar);
+      }
+    }
     let content = await this.getContent(templateFile);
     let doTCompiled = doT.template(content, options, dotDef);
+    if(options.cache_compile) {
+      thinkCache(thinkCache.VIEW_CONTENT, templateFile + '-compile', doTCompiled);
+    }
 
     return doTCompiled(tVar);
   }
-}
+};
